ba9290a297415c85dd5d03caad60173290dd5010
mizanur1326 <mizanur1326@gmail.com>
2025-05-08 19:02:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header'
+import Hero from './Hero'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default App
